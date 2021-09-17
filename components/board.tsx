a0ec06b5cdcf17a259323f5071efea5589d299e2
Igor Souza <igormcsouza@gmail.com>
2021-09-17 17:34:52 -0300

    Add functionality to check buttons

--- components/board.tsx
+++ components/board.tsx
@@ -1,22 +1,43 @@
+import { useState } from "react";
 import Square from "./square";
 
 export default function Board () {
+  const [turnPlayer, setTurnPlayer] = useState("X")
+
   return (
     <table>
       <tr>
-        <td><Square /></td>
-        <td className="vert"><Square /></td>
-        <td><Square /></td>
+        <td>
+          <Square turnPlayerName={turnPlayer} changeTurn={(playerName: string) => setTurnPlayer(playerName)}/>
+        </td>
+        <td className="vert">
+          <Square turnPlayerName={turnPlayer} changeTurn={(playerName: string) => setTurnPlayer(playerName)}/>
+        </td>
+        <td>
+          <Square turnPlayerName={turnPlayer} changeTurn={(playerName: string) => setTurnPlayer(playerName)}/>
+        </td>
       </tr>
       <tr>
-        <td className="hori"><Square /></td>
-        <td className="vert hori"><Square /></td>
-        <td className="hori"><Square /></td>
+        <td className="hori">
+          <Square turnPlayerName={turnPlayer} changeTurn={(playerName: string) => setTurnPlayer(playerName)}/>
+        </td>
+        <td className="vert hori">
+          <Square turnPlayerName={turnPlayer} changeTurn={(playerName: string) => setTurnPlayer(playerName)}/>
+        </td>
+        <td className="hori">
+          <Square turnPlayerName={turnPlayer} changeTurn={(playerName: string) => setTurnPlayer(playerName)}/>
+        </td>
       </tr>
       <tr>
-        <td><Square /></td>
-        <td className="vert"><Square /></td>
-        <td><Square /></td>
+        <td>
+          <Square turnPlayerName={turnPlayer} changeTurn={(playerName: string) => setTurnPlayer(playerName)}/>
+        </td>
+        <td className="vert">
+          <Square turnPlayerName={turnPlayer} changeTurn={(playerName: string) => setTurnPlayer(playerName)}/>
+        </td>
+        <td>
+          <Square turnPlayerName={turnPlayer} changeTurn={(playerName: string) => setTurnPlayer(playerName)}/>
+        </td>
       </tr>
     </table>
   )
--- components/square.tsx
+++ components/square.tsx
@@ -1,3 +1,22 @@
-export default function Square () {
-  return <button className="btn btn-overlay text-white square">X</button>
+import { useState } from "react"
+
+interface SquareType {
+  turnPlayerName: string,
+  changeTurn: (palyerName: string) => void
+}
+
+export default function Square ({turnPlayerName, changeTurn}: SquareType) {
+  const [player, setPlayer] = useState("")
+  
+  return (
+    <button 
+      className="btn btn-overlay text-white square" 
+      onClick={() => { 
+        setPlayer(turnPlayerName)
+        changeTurn(turnPlayerName === "X"? "O" : "X")
+      }}
+    >
+      {player}
+    </button>
+  )
 }
